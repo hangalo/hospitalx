7e12e73692e0483a67ba21228743892e26fa1305
Ary <Ary@192.168.1.7>
2020-12-11 20:27:12 +0000

    Criação da classe Medico

---
+++ src/main/java/hospitalx/modelo/Medico.java
@@ -0,0 +1,21 @@
+
+package hospitalx.modelo;
+
+
+public class Medico {
+    private int idMedico;
+    private String nomeMedico;
+    private String sobrenomMedico;
+    /*`medico`.`data_nascimento_medico`,
+    `medico`.`sexo_medico`,
+    `medico`.`email_medico`,
+    `medico`.`telefone_medico`,
+    `medico`.`rua_medico`,
+    `medico`.`casa_medico`,
+    `medico`.`bairro_medico`,
+    `medico`.`distritito_medico`,
+    `medico`.`id_municipio`
+FROM `hospitalx`.`medico`;*/
+    
+    
+}
